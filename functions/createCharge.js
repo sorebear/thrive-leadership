4a7e9677a4f3109b2fd0c29729e6ee3e41c28af0
sorebear <sorenbaird@gmail.com>
2018-08-08 21:12:18 -0700

    Checking if requiring 'stripe' in the backend is the issue

--- functions/createCharge.js
+++ functions/createCharge.js
@@ -10,36 +10,37 @@ exports.handler = (event, context, callback) => {
   const amount = requestBody.charge.amount;
   const currency = requestBody.charge.currency;
 
+  console.log('STRIPE:', stripe);
   console.log('TOKEN', token, 'AMOUNT', amount, 'CURRENCY', currency);
-  return stripe.charges.create({
-    amount,
-    currency,
-    description: 'Serverless Stripe Test Charge',
-    source: token,
-  }).then(charge => {
-    console.log(charge);
-    const response = {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: JSON.stringify({
-        message: 'Charge processed successfully!',
-        charge,
-      }),
-    };
-    callback(null, response);
-  }).catch(err => {
-    console.log(err);
-    const response = {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: JSON.stringify({
-        error: err.message,
-      }),
-    };
-    callback(null, response);
-  });
+  // stripe.charges.create({
+  //   amount,
+  //   currency,
+  //   description: 'Serverless Stripe Test Charge',
+  //   source: token,
+  // }).then(charge => {
+  //   console.log(charge);
+  //   const response = {
+  //     statusCode: 200,
+  //     headers: {
+  //       'Access-Control-Allow-Origin': '*',
+  //     },
+  //     body: JSON.stringify({
+  //       message: 'Charge processed successfully!',
+  //       charge,
+  //     }),
+  //   };
+  //   callback(null, response);
+  // }).catch(err => {
+  //   console.log(err);
+  //   const response = {
+  //     statusCode: 500,
+  //     headers: {
+  //       'Access-Control-Allow-Origin': '*',
+  //     },
+  //     body: JSON.stringify({
+  //       error: err.message,
+  //     }),
+  //   };
+  //   callback(null, response);
+  // });
 }
