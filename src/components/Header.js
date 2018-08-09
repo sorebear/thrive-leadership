df6617dcd6e7c83f583e92de2061855730b00902
Soren Baird <sorenbaird@gmail.com>
2018-08-09 14:35:36 -0700

    Updated header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = (props) => (
   <header id="header" style={props.timeout ? {display: 'none'} : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <h2>TL</h2>
     </div>
     <div className="content">
       <div className="inner">
--- src/components/Main.js
+++ src/components/Main.js
@@ -76,12 +76,6 @@ class Main extends React.Component {
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
           <ContactForm />
-          <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
-          </ul>
           {close}
         </article>
 
