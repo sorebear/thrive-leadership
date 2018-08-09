51c72ef2cbb8187eab0960468e1e8db6853c6fc5
sorebear <sorenbaird@gmail.com>
2018-08-08 20:41:05 -0700

    Added console log to test env variables

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,6 @@
 exports.handler = (event, context, callback) => {
   console.log('Does this log show up anywhere?');
+  console.log('My Test Variable', process.env.TEST_VAR);
   callback(null, {
     statusCode: 200,
     body: 'Thrive Leadership Training',
