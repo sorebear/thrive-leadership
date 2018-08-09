4e120c3d96d41a6526ee7b3840c45f0a2ee760f0
sorebear <sorenbaird@gmail.com>
2018-08-08 20:37:49 -0700

    Added a console.log to the test function

--- functions/test.js
+++ functions/test.js
@@ -1,4 +1,5 @@
 exports.handler = (event, context, callback) => {
+  console.log('Does this log show up anywhere?');
   callback(null, {
     statusCode: 200,
     body: 'Thrive Leadership Training',
