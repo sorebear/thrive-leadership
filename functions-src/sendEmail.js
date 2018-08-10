1070b1ac5772d7c7a712881813c6cb0c97474310
sorebear <sorenbaird@gmail.com>
2018-08-09 20:33:34 -0700

    Finished nodemailer tutorial

--- functions-src/sendEmail.js
+++ functions-src/sendEmail.js
@@ -7,8 +7,12 @@ exports.handler = (event, context, callback) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'caitbaird@gmail.com',
-      pass: process.env.EMAIL_PASSWORD
+      xoauth2: xoauth2.createXOAuth2Generator({
+        user: 'caitbaird@gmail.com',
+        clientId: '118365106713-di12r3mehr3kcu1of5d8619e6q6p3n1b.apps.googleusercontent.com',
+        clientSecret: process.env.GMAIL_CLIENT_SECRET,
+        refreshToken: '1/5eSLxOl_tkf0RFPfPEgWNx2wa1WwwLKSU98sP_U1z9o'
+      })
     }
   })
 
