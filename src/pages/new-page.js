ac92c0839ac7ea892ee79480fff7b9ba26e70d55
sorebear <sorenbaird@gmail.com>
2018-08-11 11:59:59 -0700

    Added back in unused new-page.js page

---
+++ src/pages/new-page.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+class NewPage extends React.Component {
+  render() {
+    const siteTitle = this.props.data.site.siteMetadata.title;
+    const siteDescription = this.props.data.site.siteMetadata.description;
+
+    return (
+      <div>
+        <Helmet>
+          <title>{siteTitle}</title>
+          <meta name="description" content={siteDescription} />
+        </Helmet>
+
+        <h1>This is a new page</h1>
+
+      </div>
+    );
+  }
+}
+
+export default NewPage;
+
+export const pageQuery = graphql`
+  query NewPageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`;
